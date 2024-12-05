ebe0a9aca50d87e66f61123ce1c6365b7b045f33
YenlinhNguyen01 <yennguye@uat.edu>
2024-12-05 02:38:38 -0700

    Add files via upload

--- userscript.js
+++ userscript.js
@@ -5,12 +5,13 @@ function check()
 
     let row1 = document.getElementById('row1')
 
-    password = password.trim()
+    password = password.trim() //Trimming passwords will remove spaces
+    //In case of future assignments or positions, it is recommanded not to input trim for passwords. If trim is enabled, the system will not accept the password with spaces
 
-    if(password.length > 4){
+    if(password.length > 4){ //Similiar to the names and passwords, if the condition is true: this code block will be executed
         Digitlength.setAttribute("checked", "checked")
         row1.style.color = "green"
-    }
+    }//set and remove Attribute are methods used to set the value of the attribute on a specfied element
     else{
         Digitlength.removeAttribute("checked")
         row1.style.color = "rgb(56, 43, 11)"
